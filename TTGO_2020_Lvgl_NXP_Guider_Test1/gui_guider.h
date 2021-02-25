ccd0f9c3b497788ad2378baf5cdfdda3e84c6f45
dingxiao88 <dingxiao88@163.com>
2021-02-25 16:05:43 +0800

    增加NXP_Guider生成界面的测试。

---
+++ TTGO_2020_Lvgl_NXP_Guider_Test1/gui_guider.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 NXP
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef GUI_GUIDER_H
+#define GUI_GUIDER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl/lvgl.h"
+#include "guider_fonts.h"
+
+typedef struct
+{
+	lv_obj_t *screen;
+	lv_obj_t *screen_img0;
+	lv_obj_t *screen_label1;
+	lv_obj_t *screen_label1_2;
+}lv_ui;
+
+void setup_ui(lv_ui *ui);
+extern lv_ui guider_ui;
+void setup_scr_screen(lv_ui *ui);
+LV_IMG_DECLARE(_pic1_150x130);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ TTGO_2020_Lvgl_NXP_Guider_Test1/setup_scr_screen.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 NXP
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "lvgl/lvgl.h"
+#include <stdio.h>
+#include "gui_guider.h"
+#include "events_init.h"
+
+
+void setup_scr_screen(lv_ui *ui){
+
+	//Write codes screen
+	ui->screen = lv_obj_create(NULL, NULL);
+
+	//Write codes screen_img0
+	ui->screen_img0 = lv_img_create(ui->screen, NULL);
+
+	//Write style LV_IMG_PART_MAIN for screen_img0
+	static lv_style_t style_screen_img0_main;
+	lv_style_init(&style_screen_img0_main);
+
+	//Write style state: LV_STATE_DEFAULT for style_screen_img0_main
+	lv_style_set_image_recolor(&style_screen_img0_main, LV_STATE_DEFAULT, lv_color_make(0xff, 0xff, 0xff));
+	lv_style_set_image_recolor_opa(&style_screen_img0_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_image_opa(&style_screen_img0_main, LV_STATE_DEFAULT, 255);
+	lv_obj_add_style(ui->screen_img0, LV_IMG_PART_MAIN, &style_screen_img0_main);
+	lv_obj_set_pos(ui->screen_img0, 43, 28);
+	lv_obj_set_size(ui->screen_img0, 150, 130);
+	lv_obj_set_click(ui->screen_img0, true);
+	lv_img_set_src(ui->screen_img0,&_pic1_150x130);
+	lv_img_set_pivot(ui->screen_img0, 0,0);
+	lv_img_set_angle(ui->screen_img0, 0);
+
+	//Write codes screen_label1
+	ui->screen_label1 = lv_label_create(ui->screen, NULL);
+	lv_label_set_text(ui->screen_label1, "Happy");
+	lv_label_set_long_mode(ui->screen_label1, LV_LABEL_LONG_BREAK);
+	lv_label_set_align(ui->screen_label1, LV_LABEL_ALIGN_CENTER);
+
+	//Write style LV_LABEL_PART_MAIN for screen_label1
+	static lv_style_t style_screen_label1_main;
+	lv_style_init(&style_screen_label1_main);
+
+	//Write style state: LV_STATE_DEFAULT for style_screen_label1_main
+	lv_style_set_radius(&style_screen_label1_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_bg_color(&style_screen_label1_main, LV_STATE_DEFAULT, lv_color_make(0xff, 0xff, 0xff));
+	lv_style_set_bg_grad_color(&style_screen_label1_main, LV_STATE_DEFAULT, lv_color_make(0xff, 0xff, 0xff));
+	lv_style_set_bg_grad_dir(&style_screen_label1_main, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
+	lv_style_set_bg_opa(&style_screen_label1_main, LV_STATE_DEFAULT, 255);
+	lv_style_set_text_color(&style_screen_label1_main, LV_STATE_DEFAULT, lv_color_make(0x35, 0xe3, 0xcf));
+	lv_style_set_text_font(&style_screen_label1_main, LV_STATE_DEFAULT, &lv_font_simsun_12);
+	lv_style_set_text_letter_space(&style_screen_label1_main, LV_STATE_DEFAULT, 2);
+	lv_style_set_pad_left(&style_screen_label1_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_right(&style_screen_label1_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_top(&style_screen_label1_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_bottom(&style_screen_label1_main, LV_STATE_DEFAULT, 0);
+	lv_obj_add_style(ui->screen_label1, LV_LABEL_PART_MAIN, &style_screen_label1_main);
+	lv_obj_set_pos(ui->screen_label1, 65, 185);
+	lv_obj_set_size(ui->screen_label1, 100, 0);
+
+	//Write codes screen_label1_2
+	ui->screen_label1_2 = lv_label_create(ui->screen, NULL);
+	lv_label_set_text(ui->screen_label1_2, "DING XIAO");
+	lv_label_set_long_mode(ui->screen_label1_2, LV_LABEL_LONG_BREAK);
+	lv_label_set_align(ui->screen_label1_2, LV_LABEL_ALIGN_CENTER);
+
+	//Write style LV_LABEL_PART_MAIN for screen_label1_2
+	static lv_style_t style_screen_label1_2_main;
+	lv_style_init(&style_screen_label1_2_main);
+
+	//Write style state: LV_STATE_DEFAULT for style_screen_label1_2_main
+	lv_style_set_radius(&style_screen_label1_2_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_bg_color(&style_screen_label1_2_main, LV_STATE_DEFAULT, lv_color_make(0xff, 0xff, 0xff));
+	lv_style_set_bg_grad_color(&style_screen_label1_2_main, LV_STATE_DEFAULT, lv_color_make(0xff, 0xff, 0xff));
+	lv_style_set_bg_grad_dir(&style_screen_label1_2_main, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
+	lv_style_set_bg_opa(&style_screen_label1_2_main, LV_STATE_DEFAULT, 255);
+	lv_style_set_text_color(&style_screen_label1_2_main, LV_STATE_DEFAULT, lv_color_make(0x05, 0x05, 0x05));
+	lv_style_set_text_font(&style_screen_label1_2_main, LV_STATE_DEFAULT, &lv_font_simsun_12);
+	lv_style_set_text_letter_space(&style_screen_label1_2_main, LV_STATE_DEFAULT, 2);
+	lv_style_set_pad_left(&style_screen_label1_2_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_right(&style_screen_label1_2_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_top(&style_screen_label1_2_main, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_bottom(&style_screen_label1_2_main, LV_STATE_DEFAULT, 0);
+	lv_obj_add_style(ui->screen_label1_2, LV_LABEL_PART_MAIN, &style_screen_label1_2_main);
+	lv_obj_set_pos(ui->screen_label1_2, 67, 213);
+	lv_obj_set_size(ui->screen_label1_2, 100, 0);
+}
