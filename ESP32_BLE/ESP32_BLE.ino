22ac481166997e4d3e706e082bb6c30d727fb3f9
dingxiao88 <dingxiao88@163.com>
2021-05-11 17:31:20 +0800

    新增ESP32_BLE测试，已成功实现与PC端python程序的BLE通讯功能。

--- ESP32_BLE/ESP32_BLE.ino
+++ ESP32_BLE/ESP32_BLE.ino
@@ -1,53 +1,188 @@
-#include "esp_bt_main.h"
-#include "esp_bt_device.h"
-#include "esp_gap_bt_api.h"
- 
-bool initBluetooth(const char *deviceName)
-{
-  if (!btStart()) {
-    Serial.println("Failed to initialize controller");
-    return false;
-  }
- 
-  if (esp_bluedroid_init()!= ESP_OK) {
-    Serial.println("Failed to initialize bluedroid");
-    return false;
-  }
- 
-  if (esp_bluedroid_enable()!= ESP_OK) {
-    Serial.println("Failed to enable bluedroid");
-    return false;
-  }
- 
-  esp_bt_dev_set_device_name(deviceName);
- 
-  esp_bt_gap_set_scan_mode(ESP_BT_SCAN_MODE_CONNECTABLE_DISCOVERABLE);
- 
-}
- 
-void printDeviceAddress() {
- 
-  const uint8_t* point = esp_bt_dev_get_address();
- 
-  for (int i = 0; i < 6; i++) {
- 
-    char str[3];
- 
-    sprintf(str, "%02X", (int)point[i]);
-    Serial.print(str);
- 
-    if (i < 5){
-      Serial.print(":");
+// #include <BTstack.h>
+// #include <stdio.h>
+// #include <SPI.h>
+
+// /* EXAMPLE_START(iBeacon): iBeacon Simulator
+//  *
+//  * @section Setup
+//  *
+//  * @text After BTstack.setup(), iBeaconConfigure() configures BTstack
+//  * to send out iBeacons Advertisements with the provided Major ID,
+//  * Minor ID and UUID.
+//  */
+// /* LISTING_START(iBeaconSetup): iBeacon Setup */
+// UUID uuid("E2C56DB5-DFFB-48D2-B060-D0F5A71096E0");
+// void setup(void){
+//     Serial.begin(115200);
+//     BTstack.setup();
+//     BTstack.iBeaconConfigure(&uuid, 4711, 2);
+//     BTstack.startAdvertising();
+// }
+// /* LISTING_END(iBeaconSetup) */
+
+// void loop(void){
+//     BTstack.loop();
+// }
+
+
+
+// #include "BluetoothSerial.h"
+
+// #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+// #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+// #endif
+
+// BluetoothSerial SerialBT;
+
+// void setup() {
+//   Serial.begin(115200);
+//   SerialBT.begin("ESP32test"); //Bluetooth device name
+//   Serial.println("The device started, now you can pair it with bluetooth!");
+// }
+
+// void loop() {
+//   if (Serial.available()) {
+//     SerialBT.write(Serial.read());
+//   }
+//   if (SerialBT.available()) {
+//     Serial.write(SerialBT.read());
+//   }
+//   delay(20);
+// }
+
+
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updated by chegewara
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
+   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+   A connect hander associated with the server starts a background task that performs notification
+   every couple of seconds.
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+uint8_t txValue = 0;
+char  BLE_Msg[256];                      //BLE发送发送缓存区
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
     }
- 
-  }
-}
- 
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+                            
+    //  Serial.println("DXDXDX");
+
+      if( rxValue.length() < 256)
+      {
+          memset(BLE_Msg, 0, sizeof(BLE_Msg));
+          for (int i = 0; i < rxValue.length(); i++)
+          {
+            BLE_Msg[i] = rxValue[i];
+            // BLE_Msg_Input.setCharAt(i,rxValue[i]);
+          }
+      }
+
+      if(rxValue[0] == 0x01)
+      Serial.println("ok");
+
+     }
+};
+
+
+
 void setup() {
   Serial.begin(115200);
- 
-  initBluetooth("DX_BLE_TEST");
-  printDeviceAddress();
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+  pCharacteristic->setCallbacks(new MyCallbacks()); 
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
- 
-void loop() {}
+
+void loop() {
+    // notify changed value
+    if (deviceConnected) {
+        // pCharacteristic->setValue((uint8_t*)&value, 4);
+        pCharacteristic->setValue((uint8_t*)&txValue, 1);
+        pCharacteristic->notify();
+        // Serial.println(value);
+        txValue++;
+        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    }
+    // // disconnecting
+    // if (!deviceConnected && oldDeviceConnected) {
+    //     delay(500); // give the bluetooth stack the chance to get things ready
+    //     pServer->startAdvertising(); // restart advertising
+    //     Serial.println("start advertising");
+    //     oldDeviceConnected = deviceConnected;
+    // }
+    // // connecting
+    // if (deviceConnected && !oldDeviceConnected) {
+    //     // do stuff here on connecting
+    //     oldDeviceConnected = deviceConnected;
+    // }
+}
