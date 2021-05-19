fc8f010632f4a97481433bbc604e23200fba8efd
dingxiao88 <dingxiao88@163.com>
2021-05-19 15:19:49 +0800

    ESP32_EPD增加图片显示功能。

--- ESP32_EPD/ESP32_EPD.ino
+++ ESP32_EPD/ESP32_EPD.ino
@@ -11,6 +11,9 @@
 #include "driver/rtc_io.h"
 
 
+#include "DX_EPD_Test.h"
+
+
 #define KEY2 26
 #define KEY1 27  
 
@@ -21,6 +24,8 @@ EPD4INC epd_drv_dx;
 char buff_dx[128];
 int good = 18;
 
+int dis_tick = 0;
+struct pl_area user_area_dx;
 
 
 void setup()
@@ -63,16 +68,19 @@ void setup()
 
   epd_drv_dx.Buf_UpdateFull(1);
 
-  // user_area.top = 180;
-  // user_area.left = 35;   
-  // user_area.width = 120;
-  // user_area.height = 16;
+  user_area_dx.top = 0;
+  user_area_dx.left = 0;   
+  user_area_dx.width = 239;
+  user_area_dx.height = 366;
+
+  epd_drv_dx.User_Img_Tran(239,366,gImage_DX_EPD_Test,S1D13541_LD_IMG_1BPP,&user_area_dx,1);
+
   
 //刷新背景
   // epd_drv.EPD_Update_Full(12000, S1D13541_LD_IMG_1BPP, gImage_InitPage);
 //写入ID
-  sprintf(buff_dx,"世界人民大团结万岁 dingxiao =%d",good);
-  epd_drv_dx.EPD_SetFount(FONT12);
+  sprintf(buff_dx,"世界人民大团结万岁 =%d",good);
+  epd_drv_dx.EPD_SetFount(FONT16);
   epd_drv_dx.DrawUTF( 35 , 180, buff_dx, 1);    //显示设备序列号
   
   epd_drv_dx.EPD4INC_HVEN();
@@ -92,6 +100,42 @@ void setup()
 
 void loop()
 {
+  dis_tick = dis_tick + 1;
+  if(dis_tick >= 10)
+  {
+    dis_tick = 0;
 
+    good = good + 1;
 
+    Serial.println("----------DrawTimeUpdata-----------");
+    epd_drv_dx.EPD4INC_Port_Reinit();      //SPI初始化
+    epd_drv_dx.EPD4INC_HVEN();
+    delay(2); 
+    epd_drv_dx.EPD_CLK_EX();               //其中时钟
+    epd_drv_dx.s1d135xx_set_epd_power(1);
+    delay(2);
+
+    user_area_dx.top = 180;
+    user_area_dx.left = 35;
+    user_area_dx.width = 200;
+    user_area_dx.height = 16;
+
+    sprintf(buff_dx,"世界人民大团结万岁 =%d",good);
+    epd_drv_dx.EPD_SetFount(FONT16);
+    epd_drv_dx.DrawUTF( 35 , 180, buff_dx, 1);    //显示设备序列号
+
+    // epd_drv.DrawTime(10,30, timeStruct.hours, timeStruct.minutes, FONT48_NUM, 1);
+
+    epd_drv_dx.EPD_UpdateUser(2, UPDATE_PARTIAL_AREA, &user_area_dx);
+
+    // epd_drv_dx.EPD_UpdateUser(1, UPDATE_FULL, NULL);
+    
+    // Serial.println("\n Epd UPDATE_PARTIAL_AREA updat  Over");
+    delay(300);
+    epd_drv_dx.EPD4INC_HVDISEN();
+    epd_drv_dx.s1d135xx_set_power_state(PL_EPDC_SLEEP);
+    epd_drv_dx.EPD_CLK_STOP();
+  }
+  
+  delay(10);
 }
