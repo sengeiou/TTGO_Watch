327afd404b4c257c7e180c63483defc70bfca9e2
dingxiao88 <dingxiao88@163.com>
2021-05-24 15:45:50 +0800

    ESP32_EPD项目修复RTC和SHT30同时访问的问题，界面增加实时时间显示。

--- ESP32_EPD/ESP32_EPD.ino
+++ ESP32_EPD/ESP32_EPD.ino
@@ -27,9 +27,8 @@
 
 
 EPD4INC epd_drv_dx;
-
 //@-创建RTC
-// I2C_BM8563 rtc(I2C_BM8563_DEFAULT_ADDRESS, Wire1);  //Wire1已在Wire.h文件中定义，为ESP32的I2C接口
+I2C_BM8563 rtc_dx(I2C_BM8563_DEFAULT_ADDRESS, Wire);
 
 //@-创建温湿度传感器
 SHT3X sht30(0x44);
@@ -45,15 +44,21 @@ struct pl_area user_area_dx;
 const char* ssid     = "wuyiyi";
 const char* password = "dingxiao";
 
+
+//@-网络授时
 // const char* ntpServer = "pool.ntp.org";   ----->ok
 // const char* ntpServer = "cn.pool.ntp.org";  ---->OK
 const char* ntpServer = "cn.ntp.org.cn";
 
-
+//@-设置时区参数
 const long  gmtOffset_sec = 7 * 3600;
 const int   daylightOffset_sec = 3600;
 
+int wifi_connect_tick = 0;
+int wifi_connect_flag = 0;  //@-0:没有连接  1:连接成功  2:连接超时
 
+I2C_BM8563_DateTypeDef dx_dateStruct;
+I2C_BM8563_TimeTypeDef dx_timeStruct;
 
 
 void setup()
@@ -68,16 +73,17 @@ void setup()
   SPIFFS.begin();
   Serial.println("\nSPIFFS on ");  
 
-    if(sht30.get()==0){
-    Serial.print("Temperature in Celsius : ");
-    Serial.println(sht30.cTemp);
-    Serial.print("Temperature in Fahrenheit : ");
-    Serial.println(sht30.fTemp);
-    Serial.print("Relative Humidity : ");
-    Serial.println(sht30.humidity);
-    Serial.println();
-  }
+  Wire.begin(21, 22);
 
+  //   if(sht30.get()==0){
+  //   Serial.print("Temperature in Celsius : ");
+  //   Serial.println(sht30.cTemp);
+  //   Serial.print("Temperature in Fahrenheit : ");
+  //   Serial.println(sht30.fTemp);
+  //   Serial.print("Relative Humidity : ");
+  //   Serial.println(sht30.humidity);
+  //   Serial.println();
+  // }
 
   Serial.println("----------Start_Init_EPD()-----------");
   
@@ -149,13 +155,28 @@ void setup()
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    wifi_connect_tick = wifi_connect_tick + 1;
+    if(wifi_connect_tick > 30)
+    {
+      wifi_connect_tick = 0;
+      wifi_connect_flag = 2;  //wifi连接超时
+      break;
+    }
   }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  
-  // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+  if(wifi_connect_flag != 2)
+  {
+    wifi_connect_flag = 1;
+    Serial.println("");
+    Serial.println("WiFi connected.");
+  }
+
+  //@-wifi连接成功
+  if(wifi_connect_flag == 1)
+  {
+    // Init and get the time
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    printLocalTime();
+  }
 
   //disconnect WiFi as it's no longer needed
   WiFi.disconnect(true);
@@ -182,6 +203,48 @@ void printLocalTime(){
   Serial.println(timeinfo.tm_hour);
   Serial.println(timeinfo.tm_min);
   Serial.println(timeinfo.tm_sec);
+
+  SetRTC(timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday, timeinfo.tm_wday, 
+         timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+}
+
+
+void SetRTC(uint16_t rtc_year,uint8_t rtc_month,uint8_t rtc_date,uint8_t rtc_weekDay,uint8_t rtc_hours,uint8_t rtc_minutes,uint8_t rtc_seconds)
+{
+  rtc_dx.begin();
+  // Set RTC Date
+  I2C_BM8563_DateTypeDef set_dateStruct;
+  set_dateStruct.weekDay = rtc_weekDay;
+  set_dateStruct.month = rtc_month;
+  set_dateStruct.date = rtc_date;
+  set_dateStruct.year = rtc_year;
+  rtc_dx.setDate(&set_dateStruct);
+
+  // Set RTC Time
+  I2C_BM8563_TimeTypeDef set_timeStruct;
+  set_timeStruct.hours   = rtc_hours;
+  set_timeStruct.minutes = rtc_minutes;
+  set_timeStruct.seconds = rtc_seconds;
+  rtc_dx.setTime(&set_timeStruct);
+  }
+
+
+void  ReadRTC(void)
+{
+  rtc_dx.begin();
+  // Get RTC
+  rtc_dx.getDate(&dx_dateStruct);
+  rtc_dx.getTime(&dx_timeStruct);
+
+  // Print RTC
+  Serial.printf("%04d/%02d/%02d %02d:%02d:%02d\n",
+                dx_dateStruct.year,
+                dx_dateStruct.month,
+                dx_dateStruct.date,
+                dx_timeStruct.hours,
+                dx_timeStruct.minutes,
+                dx_timeStruct.seconds
+               );
 }
 
 void loop()
@@ -193,7 +256,9 @@ void loop()
 
     good = good + 1;
 
-    sht30.get();
+    sht30.get();  //获得数据
+    delay(5);
+    ReadRTC();
 
     Serial.println("----------DrawTimeUpdata-----------");
     epd_drv_dx.EPD4INC_Port_Reinit();      //SPI初始化
@@ -206,11 +271,20 @@ void loop()
     user_area_dx.top = 180;
     user_area_dx.left = 35;
     user_area_dx.width = 200;
-    user_area_dx.height = 16;
+    user_area_dx.height = 50;
 
-    sprintf(buff_dx,"温度:%0.1f  湿度:%0.1f", sht30.cTemp, sht30.humidity);
     epd_drv_dx.EPD_SetFount(FONT16);
-    epd_drv_dx.DrawUTF( 35 , 180, buff_dx, 1);    //显示设备序列号
+    sprintf(buff_dx,"温度:%0.1f  湿度:%0.1f", sht30.cTemp, sht30.humidity);
+    // if(wifi_connect_flag == 1)
+    // sprintf(buff_dx,"WIFI连接成功 =%d",good);
+    // else if(wifi_connect_flag != 1)
+    // sprintf(buff_dx,"WIFI没有连接 =%d",good);
+    epd_drv_dx.DrawUTF( 35 , 180, buff_dx, 1);    
+
+
+    sprintf(buff_dx,"时间:%2d:%2d:%2d", dx_timeStruct.hours, dx_timeStruct.minutes, dx_timeStruct.seconds);
+    epd_drv_dx.DrawUTF( 35 , 200, buff_dx, 1);  
+
 
     // epd_drv.DrawTime(10,30, timeStruct.hours, timeStruct.minutes, FONT48_NUM, 1);
 
